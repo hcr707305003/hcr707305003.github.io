5c327a97430dd140c8877f0e7af6dbadf0e4040a
shiroi <707305003@qq.com>
2026-08-19 11:44:18 +0800

    Site updated: 2026-08-19 11:44:16

--- dist/music.js
+++ dist/music.js
@@ -10,21 +10,21 @@ const ap = new APlayer({
       cover: 'https://p3fx.kgimg.com/stdmusic/240/20210113/20210113041630995040.jpg',
     },
     {
-      name: "oblivious",
-      artist: 'Kalafina',
-      url: '/music/oblivious.mp3',
-      cover: '//y.qq.com/music/photo_new/T002R300x300M000001eNSk10EjpBz_2.jpg?max_age=2592000',
+      name: "楽園PROJECT",
+      artist: 'Ray',
+      url: '/music/楽園PROJECT.mp3',
+      cover: 'http://p2.music.126.net/KdOoY50mYJcYTMiGqgpVtQ==/6050612488066929.jpg?param=130y130',
     },
     {
-      name: "being",
-      artist: 'KOTOKO',
-      url: '/music/being.mp3',
-      cover: '//y.qq.com/music/photo_new/T002R300x300M00000283Pae1B412l_1.jpg?max_age=2592000',
+      name: "PHOSPHOR",
+      artist: '宮崎羽衣',
+      url: '/music/PHOSPHOR.mp3',
+      cover: 'http://p1.music.126.net/_yd5-yov50S7nTfnil7pDA==/109951173175249065.jpg?param=130y130',
     },
     {
       name: "Now Loading!!!!",
       artist: '高田忧希、山口爱、户田惠、竹尾步美',
-      url: '/music/now-loading.mp3',
+      url: '/music/Now Loading!!!!.m4a',
       cover: '//y.qq.com/music/photo_new/T002R300x300M0000033MkZL3GaqMv_1.jpg?max_age=2592000',
     }
   ]
